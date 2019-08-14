1907d511409460530d2c402bbdd78af023b88ff8
philip <philipakpeki@gmail.com>
2019-08-14 03:52:06 +0100

    manually configuring the oauth2 authenticationFilter

--- src/main/java/com/devphilip/simple/oauth2/SpringOauth2SimpleApplication.java
+++ src/main/java/com/devphilip/simple/oauth2/SpringOauth2SimpleApplication.java
@@ -1,20 +1,41 @@
 package com.devphilip.simple.oauth2;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.CompositeFilter;
 
 @SpringBootApplication
-@EnableOAuth2Sso
+@EnableOAuth2Client
 @RestController
 public class SpringOauth2SimpleApplication extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	OAuth2ClientContext oAuth2ClientContext;
 
 	@RequestMapping("/user")
 	public Principal user(Principal principal) {
@@ -35,7 +56,42 @@ public class SpringOauth2SimpleApplication extends WebSecurityConfigurerAdapter 
 			.anyRequest()
 				.authenticated()
 			.and().logout().logoutSuccessUrl("/").permitAll()
-			.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+			.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			.and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
 	}
 	
+	private Filter ssoFilter() {
+		
+		CompositeFilter filter = new CompositeFilter();
+		List<Filter> filters = new ArrayList<>();
+		
+		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
+		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oAuth2ClientContext);
+		facebookFilter.setRestTemplate(facebookTemplate);
+		UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId());
+		tokenServices.setRestTemplate(facebookTemplate);
+		facebookFilter.setTokenServices(tokenServices);
+		return facebookFilter;
+	}
+
+	@Bean
+	@ConfigurationProperties("facebook.client")
+	private AuthorizationCodeResourceDetails facebook() {
+		return new AuthorizationCodeResourceDetails();
+	}
+	
+	@Bean
+	@ConfigurationProperties("facebook.resource")
+	private ResourceServerProperties facebookResource() {
+		return new ResourceServerProperties();
+	}
+	
+	@Bean
+	public FilterRegistrationBean<OAuth2ClientContextFilter> oAuth2ClientContextFilterRegistration(OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<OAuth2ClientContextFilter>();
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+		return registration;
+	}
+
 }
